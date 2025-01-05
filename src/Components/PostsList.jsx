b29bbb48e4ac54770ec8cc44b4e262bc2c7efe8e
haegi <b_haegi@naver.com>
2025-01-05 19:33:05 +0900

    백엔드 더미데이터 추가 및 get,post요청 추가

--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css';
 import Modal from "./Modal";
 
 function PostsList({isPosting,hideModalHandler}){
-
     const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        async function fetchPosts(){
+            const response = await fetch('http://localhost:8080/posts');
+            const data = await response.json();
+            setPosts(data.posts);
+        }
+        fetchPosts();
+
+    },[]);
+
+    
 
     const addPostHandler = (post) => {
+        fetch('http://localhost:8080/posts',{
+            method: 'POST',
+            body: JSON.stringify(post),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         setPosts((prevPosts) => {
             return [post, ...prevPosts];
         });
     }
-    
-
     return(
         <>
             {isPosting && <Modal hideModalHandler={hideModalHandler}>
@@ -23,12 +38,19 @@ function PostsList({isPosting,hideModalHandler}){
                 addPostHandler={addPostHandler}/>
                 </Modal>}
             
-            <ul className={classes.posts}>
+            {posts.length === 0 && (
+                <div style={{textAlign: 'center', color: '#fff'}}>
+                <h2>No posts available. Start adding some!</h2>
+                </div>
+            )}
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
                 {posts.map((post,index) => (
                     <Post key={index} author={post.author} body={post.body} />
                 ))}
-                <Post author="Haegi" body="HRRRR" />
             </ul>
+            )}
+            
         </>
     )
 }
